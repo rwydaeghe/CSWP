dd584be31c17fbc064af80c822e6ca7645a5c58a
rbnwy <rbnwy12@gmail.com>
2019-11-14 11:13:02 +0100

    wip

--- FEM/FEM.m
+++ FEM/FEM.m
@@ -1,11 +1,15 @@
 function FEM()
     clf
     addpath('./DistMesh') %Je kunt nu ook scriptjes vinden in die grote folder voor meshes
-    z_max = 1;
+    z_max = pi;
     r_max = 1;
     vertices_length = 0.15;
     coord_fix=[0,0;0,boundary(0);z_max,0;z_max,boundary(z_max)];
     [V,F]=distmesh2d(inline('dfct(rz,@(rz) boundary(rz(:,1)))','rz'),@huniform,vertices_length,[0,0;z_max,r_max],coord_fix);
-    V;
-    F;
+    
+    
+    
+    
+    %a=[1,2,3];
+    %a(mod(4,3))
 end
--- FEM/boundary.m
+++ FEM/boundary.m
@@ -1,6 +1,6 @@
 function r = boundary(z)
-r=cos(z);
+r=sin(z);
 %in z=0 moet r=0
-if z==0 & r~=0
-    disp('Not a valid boundary function because r is not 0 for z=0. Will proceed anyways.')
-end
+%if z==0 & r~=0
+%    disp('Not a valid boundary function because r is not 0 for z=0. Will proceed anyways.')
+%end
