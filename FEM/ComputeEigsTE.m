1b234ce8fd6b005b88056c4af70134b2af094506
rbnwy <rbnwy12@gmail.com>
2019-12-04 00:05:07 +0100

    Bsxfun is nu gevectorizeerd met custom dubbele windows en heeft betere performance

--- FEM/ComputeEigsTE.m
+++ FEM/ComputeEigsTE.m
@@ -1,4 +1,5 @@
 function ComputeEigsTE(N)
+    %N=180;
     %clf
     %close all
     %addpath('./DistMesh') %Je kunt nu ook scriptjes vinden in die grote folder voor meshes
@@ -55,20 +56,7 @@ function ComputeEigsTE(N)
     %Write F as function of E      
     Fedge=zeros(3,length(F)); Coordn=[I(:),J(:),e(:)];
     ME2=ndSparse.build(Coordn(e(:)~=0,:),1,[length(V),length(V),length(F)]);
-    Fedge=find3D(findPattern(find3D(ME2+permute(ME2,[2,1,3]),'3d'), E.',length(F)),'2d');
-    %Fedge=find3D(bsxfun(@eq,find3D(ME2+permute(ME2,[2,1,3]),'3d'), E.'),'2d');
-    %Analyse visualisatie voor windows:
-    %{
-    hold on
-    plot([1:length(Fedge)],mean(Fedge))
-    hold on
-    plot([1:length(Fedge)],max(Fedge)-min(Fedge))
-    m=mean(Fedge); std=max(Fedge)-min(Fedge); 
-    output=[m(end)/length(F),max(std)/sqrt(length(F))];
-    %Conclusions:
-    %the evolution of the means goes as 1:2.5*length(F) with F
-    %the max of (max-min) is <2*sqrt(length(F))
-    %}
+    Fedge=find3D(findPattern(find3D(ME2+permute(ME2,[2,1,3]),'3d'), E.',length(F),N),'2d');
     I=F([1,2,3,1,2,3,1,2,3],:); J=F([1,1,1,2,2,2,3,3,3],:);
     I_edge=Fedge([1,2,3,1,2,3,1,2,3],:); J_edge=Fedge([1,1,1,2,2,2,3,3,3],:);
     
@@ -96,10 +84,10 @@ function ComputeEigsTE(N)
     %figure('Name','G_node')
     %spy(G_node)
         
-    %length(E)-length(F)+1 %eig gwn #nodes volgens euler
-    %sum(svd(full(M_edge))<0.01)
-    [Eedge,D]=eigs(M_edge,G_edge,3); %also not quite O(n)...
-    D
+    length(E)-length(F)+1 %eig gwn #nodes volgens euler
+    sum(svd(full(M_edge))<0.01)
+    [Eedge,D]=eigs(M_edge,G_edge,5); %also not quite O(n)...
+    sparse(D)
     %Eedge
     
     V=V.'; %to do: write viewMesh so this isn't necessary(?)
@@ -138,34 +126,47 @@ function out = find2D(X) %for elegance in one line and efficient double-element 
     [x,y]=find(X); out = (x+j*y);
 end
 
-function out=findPattern(A,B,lenF) %find patternS A in data B and put them in array
-    %bedoeling: O(n) maken van out=bsxfun(@eq,A,B)
-    %%{
-    %Conclusions:
-    %the evolution of the means goes as 1:2.5*length(F) with F (at most)
-    %the max of (max-min) is <2*sqrt(length(F))=length(B) (at most) logical because
-    %that's just length(V)/length(E) for big meshes
-    
-    %create windows
-    mean=[1:length(B)/lenF:length(B)];
-    expon=0.6; %Voor rechthoek: 0.5 theoretisch, 0.6 praktisch. Als algemene mesh, stel expon 1 (maw, heel dit algoritme is nutteloos, je kunt al evengoed bsx one-line gebruiken)
-    halfwindowsize=length(B)^expon*ones(1,length(mean));
-    windowstart=mean-1.7*halfwindowsize; windowstart(windowstart<1)=1; windowstart=ceil(windowstart); %factor 1.7 via visualisatie trial en error...
-    windowend=mean+halfwindowsize; windowend(windowend>length(B))=length(B); windowend=floor(windowend);
-    windowsize=windowend-windowstart;
-    
-    %findPattern in windows using sparse matrices
-    B=B.*ones(1,length(B),lenF);
-    out_prov=zeros(3,length(B),lenF); %provisoir
-    for u=1:lenF
-        B(1,1:windowstart(u)-1,u)=0; B(1,windowend(u)+1:end,u)=0;
+function out=findPattern(A,B,lenF,N) %find patternS A in data B and put them in array
+    %bedoeling: O(n) maken van out=bsxfun(@eq,A,B) 
+    if N <=7
+        out=bsxfun(@eq,A,B); %algoritmes werken eigenlijk enkel voor grote N. Bovendien is dit minder overlay = sneller
+    else
+        %create windows
+        axis=[1:length(B)/lenF:length(B)];
+        mean2=axis-[1:1:lenF]/lenF*N+ones(1,lenF)*N;
+        mean1=mean2-ones(1,lenF)*(3.045*N-3.88); knik=floor(3*N*lenF/length(B)); mean1=[axis(1:knik)/axis(knik)*mean1(knik) mean1(knik+1:end)];
+        if N>=50
+            halfwindowsize=N/10;
+        else
+            halfwindowsize=5;
+        end
+        if N>=41
+            %another speed hack for large N
+            window1start=mean1; window1start(window1start<1)=1; window1start(window1start>length(B))=length(B); window1start=ceil(window1start);
+        else
+            window1start=mean1-halfwindowsize; window1start(window1start<1)=1; window1start(window1start>length(B))=length(B); window1start=ceil(window1start);
+        end
+        window1end=mean1+halfwindowsize; window1end(window1end<1)=1; window1end(window1end>length(B))=length(B); window1end=ceil(window1end);
+        window2start=mean2-halfwindowsize; window2start(window2start<1)=1; window2start(window2start>length(B))=length(B); window2start=ceil(window2start);
+        window2end=mean2+halfwindowsize; window2end(window2end<1)=1; window2end(window2end>length(B))=length(B); window2end=ceil(window2end);
+        coordwindow1=[]; coordwindow2=[]; 
+        for u=1:lenF
+            newbit1=window1start(u):window1end(u);
+            coordwindow1=[coordwindow1 [newbit1;u*ones(size(newbit1))]];
+            newbit2=window2start(u):window2end(u);
+            coordwindow2=[coordwindow2 [newbit2;u*ones(size(newbit2))]];
+        end    
+        coordwindows=[coordwindow1 coordwindow2]; sz=size(coordwindows); coordwindows=[coordwindows; ones(1,sz(2))];
+        
+        %findPattern in windows using sparse matrices
+        B=ndSparse.build(coordwindows([3,1,2],:).', B(1,coordwindows(1,:)).');
+        out=ndSparse(bsxfun(@eq,A,B));
+        %Visualisatie
+        %{
+        plot(axis,[mean1;window1start;window1end;mean2;window2start;window2end])
+        hold on
+        out_vis=mod(find(sum(out,1))-1,length(B))+1;    
+        scatter([1:length(B)/(length(out_vis)+1):length(B)],out_vis.','.')
+        %}
     end
-    out=ndSparse(bsxfun(@eq,A,ndSparse(B)));
-    %Visualisatie
-    %%{
-    plot(mean,[mean;windowstart;windowend])
-    hold on
-    out_vis=mod(find(sum(out,1))-1,length(B))+1;    
-    scatter([1:length(B)/(length(out_vis)+1):length(B)],out_vis.')
-    %%}    
 end
