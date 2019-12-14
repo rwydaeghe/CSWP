3556111db5a6013ff9e903066207624a5397407b
rbnwy <rbnwy12@gmail.com>
2019-12-14 17:41:01 +0100

    Voor het eerst stabiele eigenwaarden. RVWn voorlopig dubbele dirichlet en iets andere implementatie (met nul rij/kolom)

--- FEM/ComputeEigsTE.m
+++ FEM/ComputeEigsTE.m
@@ -1,6 +1,8 @@
 function ComputeEigsTE(N)
-    %N=120;
-    clf; close all; addpath('./DistMesh'); %Je kunt nu ook scriptjes vinden in die grote folder voor meshes
+    %N=40;
+    clf; 
+    %close all; 
+    addpath('./DistMesh'); %Je kunt nu ook scriptjes vinden in die grote folder voor meshes
     
     %% Create and analyze mesh
     %create V and F
@@ -14,11 +16,11 @@ function ComputeEigsTE(N)
         coord_fix=[0,0;0,boundary(0);z_max,0;z_max,boundary(z_max)];
         [V,F]=distmesh2d(inline('dfct(rz,@(rz) boundary(rz(:,1)))','rz'),@huniform,vertices_length,[0,0;z_max,r_max],coord_fix);
     end
-    V=V.'; F=F.';
+    V=V.'; F=F.';    
     F(:,1:2:end)=F([1,3,2],1:2:end); %tang cont
     
     %Create E
-    I=F([1,2,3,1,2,3,1,2,3],:); J=F([1,1,1,2,2,2,3,3,3],:);
+    i_index=[1,2,3,1,2,3,1,2,3];j_index=[1,1,1,2,2,2,3,3,3]; I=F(i_index,:); J=F(j_index,:);
     e=zeros(9,length(F)); e([2,3,6],:)=1;
     ME1=sparse(I,J,e,length(V),length(V)); 
     E_boundary=find2D(triu(ME1+ME1.')==1); E_internal=find2D(triu(ME1+ME1.')==2); E=find2D(triu(ME1+ME1.'));
@@ -47,7 +49,7 @@ function ComputeEigsTE(N)
     ME2=ndSparse.build(Coordn(e(:)~=0,:),1,[length(V),length(V),length(F)]);
     Fedge=find3D(findPattern(find3D(ME2+permute(ME2,[2,1,3]),'3d'), E.',length(F),N),'2d'); %Dit algoritme behoudt de alternerende pos of neg orientatie
     Fedge([1,2,3],2:2:end)=Fedge([3,1,2],2:2:end); %reshape(E(Fedge(:)),3,[])==F(2,:)+j*F(3,:) %(onschuldige) even permutatie zodat Fedge(1,:)<->F(1,:) en bewijs hiervoor  
-
+    
     %% Additional variables. 
     %Pre-allocate to increase performance
     z=[V(1,F(1,:));V(1,F(2,:));V(1,F(3,:))]; r=[V(2,F(1,:));V(2,F(2,:));V(2,F(3,:))];
@@ -63,63 +65,84 @@ function ComputeEigsTE(N)
     A(i,:)=(b(s(i+1),:).*c(s(i+2),:)-b(s(i+2),:).*c(s(i+1),:))/2; %idem for all i
     %a(:,2:2:end)=-a([1,3,2],2:2:end); b(:,2:2:end)=-b([1,3,2],2:2:end); c(:,2:2:end)=-c([1,3,2],2:2:end);
     
-    %%{
+    %{
     for i = 1:3
         for n=1:length(F)
             %idee vectorieel en function handle opchrijven als str. Die dan
             %omzetten naar code wanneer nodig
-            w_nodes{n,i}=@(z,r) (a(s(i),n)+b(s(i),n)*z+c(s(i),n)*r)./(2*A(i,n));
+            %Let op ik ben NIET zeker of ik hier abs(A) moet doen...
+            w_nodes{n,i}=@(z,r) (a(s(i),n)+b(s(i),n)*z+c(s(i),n)*r)./(2*abs(A(i,n)));
             w_edges_z{n,i}=@(z,r) (a(s(i+1),n).*b(s(i+2),n)-a(s(i+2),n).*b(s(i+1),n)+(c(s(i+1),n).*b(s(i+2),n)-c(s(i+2),n).*b(s(i+1),n))*r)./(4*A(i,n).^2);
             w_edges_r{n,i}=@(z,r) (a(s(i+1),n).*c(s(i+2),n)-a(s(i+2),n).*c(s(i+1),n)+(b(s(i+1),n).*c(s(i+2),n)-b(s(i+2),n).*c(s(i+1),n))*z)./(4*A(i,n).^2);
-            w_edges_z_total{n,1}=@(z,r) w_edges_z{n,1}(z,r)+w_edges_z{n,2}(z,r)+w_edges_z{n,3}(z,r);
-            w_edges_r_total{n,1}=@(z,r) w_edges_r{n,1}(z,r)+w_edges_r{n,2}(z,r)+w_edges_r{n,3}(z,r);    
+            %w_edges_z_total{n,1}=@(z,r) w_edges_z{n,1}(z,r)+w_edges_z{n,3}(z,r)+w_edges_z{n,3}(z,r);
+            %w_edges_r_total{n,1}=@(z,r) w_edges_r{n,1}(z,r)+w_edges_r{n,3}(z,r)+w_edges_r{n,3}(z,r);    
         end
     end
     %}
     i=[1,2,3];
     
     %% Build matrices and find eigenvalues
-    I_edge=Fedge([1,2,3,1,2,3,1,2,3],:); J_edge=Fedge([1,1,1,2,2,2,3,3,3],:);
+    I_edge=Fedge(i_index,:); J_edge=Fedge(j_index,:);
     
     m_edge=zeros(9,length(F));
-    m_edge([1:9],:)=ones(9,1).*(r(1,:)+r(2,:)+r(3,:))./(2*A(1,:));
+    m_edge(1:9,:)=ones(9,1).*(r(1,:)+r(2,:)+r(3,:))./(2*abs(A(1,:)));%let op eigenlijk is A in absolute waarde. Je kunt dat ook nagaan uit symmetrieredenen LL
     %m_edge([2,3],:)=m_edge([3,2],:); %tangential cont
     %m_edge(:,2:2:end)=-m_edge(:,2:2:end); %tangential continuity
     %m_edge([1,5,9],2:2:end)=-m_edge([1,5,9],2:2:end); %tangential continuity
     M_edge=sparse(I_edge,J_edge,m_edge,length(E),length(E));
 
     g_edge=zeros(9,length(F));
-    g_edge([1,5,9],:)=((b(s(i+2),:).^2+c(s(i+2),:).^2).*(2*r(s(i),:)+6*r(s(i+1),:)+2*r(s(i+2),:))-2*(b(s(i+1),:).*b(s(i+2),:)+c(s(i+1),:).*c(s(i+2),:)).*(r(s(i),:)+2*r(s(i+1),:)+2*r(s(i+2),:))+(b(s(i+1),:).^2+c(s(i+1),:).^2).*(2*r(s(i),:)+2*r(s(i+1),:)+6*r(s(i+2),:)))./(240*A(i,:));
-    g_edge([2,6,3],:)=((b(s(i+2),:).*b(s(i),:)+c(s(i+2),:).*c(s(i),:)).*(r(s(i),:)+2*r(s(i+1),:)+2*r(s(i+2),:))-(b(s(i+2),:).^2+c(s(i+2),:).^2).*(2*r(s(i),:)+2*r(s(i+1),:)+r(s(i+2),:))-(b(s(i+1),:).*b(s(i),:)+c(s(i+1),:).*c(s(i),:)).*(2*r(s(i),:)+2*r(s(i+1),:)+6*r(s(i+2),:))+(b(s(i+1),:).*b(s(i+2),:)+c(s(i+1),:).*c(s(i+2),:)).*(2*r(s(i),:)+r(s(i+1),:)+2*r(s(i+2),:)))./(240*A(i,:))./(240*A(i,:));
-    %tangential cont:
+    g_edge([1,5,9],:)=((b(s(i+2),:).^2+c(s(i+2),:).^2).*(2*r(s(i),:)+6*r(s(i+1),:)+2*r(s(i+2),:))-2*(b(s(i+1),:).*b(s(i+2),:)+c(s(i+1),:).*c(s(i+2),:)).*(r(s(i),:)+2*r(s(i+1),:)+2*r(s(i+2),:))+(b(s(i+1),:).^2+c(s(i+1),:).^2).*(2*r(s(i),:)+2*r(s(i+1),:)+6*r(s(i+2),:)))./(240*abs(A(i,:)));
+    g_edge([2,6,3],:)=((b(s(i+2),:).*b(s(i),:)+c(s(i+2),:).*c(s(i),:)).*(r(s(i),:)+2*r(s(i+1),:)+2*r(s(i+2),:))-(b(s(i+2),:).^2+c(s(i+2),:).^2).*(2*r(s(i),:)+2*r(s(i+1),:)+r(s(i+2),:))-(b(s(i+1),:).*b(s(i),:)+c(s(i+1),:).*c(s(i),:)).*(2*r(s(i),:)+2*r(s(i+1),:)+6*r(s(i+2),:))+(b(s(i+1),:).*b(s(i+2),:)+c(s(i+1),:).*c(s(i+2),:)).*(2*r(s(i),:)+r(s(i+1),:)+2*r(s(i+2),:)))./(240*abs(A(i,:)));
+    %equiv tangential cont:
     %switch in 2<->3 and 5<->9 for tangential continuity every other triangle
     %g_edge([2,3,5,9],2:2:end)=g_edge([3,2,9,5],2:2:end);
     g_edge([4,7,8],:)=g_edge([2,3,6],:); %interaction integrals are symmetric in a triangle's edge indices
     G_edge=sparse(I_edge,J_edge,g_edge,length(E),length(E));
     
     g_node=zeros(9,length(F));
-    g_node([1,5,9],:)=(3*r(s(i),:)+r(s(i+1),:)+r(s(i+2),:))./(30*A(i,:));
-    g_node([2,6,3],:)=(2*r(s(i),:)+2*r(s(i+1),:)+r(s(i+2),:))./(60*A(i,:));
+    g_node([1,5,9],:)=(3*r(s(i),:)+r(s(i+1),:)+r(s(i+2),:))./(30*abs(A(i,:)));
+    g_node([2,6,3],:)=(2*r(s(i),:)+2*r(s(i+1),:)+r(s(i+2),:))./(60*abs(A(i,:)));
     g_node([4,7,8],:)=g_node([2,3,6],:); %interaction integrals are symmetric in a triangle's edge indices
     G_node=sparse(I,J,g_node,length(V),length(V));
+    
+    m_node=zeros(9,length(F));
+    m_node(1:9,:)=c(i_index,:)/6+c(j_index,:)/6+(c(i_index,:).*c(j_index,:)+b(i_index,:).*b(j_index,:))./(2*abs(A(i_index,:))).*(r(1,:)+r(2,:)+r(3,:))/6;
+    M_node=sparse(I,J,m_node,length(V),length(V));
         
     %RVWn
-    G_edge(E_boundary_edge(:,4),:)=[];
-    G_edge(:,E_boundary_edge(:,4))=[];
-    M_edge(E_boundary_edge(:,4),:)=[];
-    M_edge(:,E_boundary_edge(:,4))=[];
-    %G_edge(E_boundary_edge(:,3),:)=[];
-    %G_edge(:,E_boundary_edge(:,3))=[];
-    %M_edge(E_boundary_edge(:,3),:)=[];
-    %M_edge(:,E_boundary_edge(:,3))=[];
+    %%{
+    sz1=size(G_edge,1); sz2=size(E_boundary_edge(:,4),1);
+    G_edge(E_boundary_edge(:,4),:)=zeros(sz2,sz1);
+    G_edge(:,E_boundary_edge(:,4))=zeros(sz1,sz2);
+    M_edge(E_boundary_edge(:,4),:)=zeros(sz2,sz1);
+    M_edge(:,E_boundary_edge(:,4))=zeros(sz1,sz2);
+    for n=1:size(E_boundary_edge(:,4),1)
+        M_edge(E_boundary_edge(n,4),E_boundary_edge(n,4))=1;
+        %G_edge(E_boundary_edge(n,4),E_boundary_edge(n,4))=1;
+    end
     
-    
+    sz1=size(G_edge,1); sz2=size(E_boundary_edge(:,3),1);
+    G_edge(E_boundary_edge(:,3),:)=zeros(sz2,sz1);
+    G_edge(:,E_boundary_edge(:,3))=zeros(sz1,sz2);
+    M_edge(E_boundary_edge(:,3),:)=zeros(sz2,sz1);
+    M_edge(:,E_boundary_edge(:,3))=zeros(sz1,sz2);
+    for n=1:size(E_boundary_edge(:,3),1)
+        M_edge(E_boundary_edge(n,3),E_boundary_edge(n,3))=1;
+        %G_edge(E_boundary_edge(n,3),E_boundary_edge(n,3))=1;
+    end
+    %}
+
     length(E)-length(F)+1 %eig gwn #nodes volgens euler
     sum(svd(full(M_edge))<0.01)
     %[Eedge,D]=eig(full(M_edge),500*full(G_edge)); %blijkaar zou dit een juister resultaat geven dan eigs
-    eig(full(G_edge)) %is not positive definite bcs of orientation shifts. this leads to complex eigs
-    [Eedge,D]=eigs(M_edge,500*G_edge); %also not quite O(n)... %(5*pi)^2=250 en we willen geen complexe getallen
-    diag(D)
+    myeigs=eig(full(M_edge),full(G_edge));
+    sum(myeigs<1)
+    %plot([1:1:size(myeigs)],myeigs)
+    [Eedge,D]=eig(full(M_edge),full(G_edge)); %also not quite O(n)... %(5*pi)^2=250 en we willen geen complexe getallen
+    dnnz=sort(D(D>1)); 
+    plot([1:1:size(sort(myeigs))],sort(myeigs))
+    dnnz(1:5)
     %sparse(D./max(D(:)))
     
     V=V.'; %to do: write viewMesh so this isn't necessary(?)
@@ -128,10 +151,10 @@ function ComputeEigsTE(N)
     %%{
     if meshType == 'cylinder'
         if (Nz+Nr)/2 < 11
-            figure('Name','Mesh and basis functions')
-            viewMeshandBasisFcts(V, F, w_nodes, w_edges_z, w_edges_r, 1, 1, 1/(6*(Nz+Nr)/2),z_max,r_max)
-            figure('Name','Mesh and basis functions2')
-            viewMeshandBasisFcts(V, F, w_nodes, w_edges_z, w_edges_r, 2, 1, 1/(6*(Nz+Nr)/2),z_max,r_max)
+            %figure('Name','Mesh and basis functions')
+            %viewMeshandBasisFcts(V, F, w_nodes, w_edges_z_total, w_edges_r_total, 1, 1, 1/(6*(Nz+Nr)/2),z_max,r_max)
+            %figure('Name','Mesh and basis functions2')
+            %viewMeshandBasisFcts(V, F, w_nodes, w_edges_z, w_edges_r, 5, 3, 1/(6*(Nz+Nr)/2),z_max,r_max)
         else
             fprintf('are you sure you want to display so many triangles? (%d,%d) \n', Nz, Nr)
         end
